3c3c786368db61fffb5c28cc97db17d975a374db
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-02-25 23:41:49 +0500

    Added requirements.txt

--- newyearproject/newyearproject/settings.py
+++ newyearproject/newyearproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import newyearproject.newyearproject.data as data
+import data
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = data.myemail
-EMAIL_HOST_PASSWORD = data.mypassword
+# EMAIL_HOST_USER = data.myemail
+# EMAIL_HOST_PASSWORD = data.mypassword
